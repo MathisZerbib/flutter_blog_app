e65e2a8cd86fa66ec205fd51fb12c084ff7fe6b2
MathisZerbib <mathis.zerbib@gmail.com>
2023-11-09 11:57:26 +0100

    add api jsonplaceholder fetch

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,12 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+Future<List<dynamic>> fetchPosts() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to fetch posts');
+  }
+}
